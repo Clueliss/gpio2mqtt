00b374f8ced7805995cdddc0ed6a447a35d64dc2
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-01-21 18:57:50 +0100

    replaced run by deploy

--- run.sh
+++ run.sh
@@ -5,9 +5,20 @@ TARGET=arm-unknown-linux-gnueabihf # Pi 0/1
 
 # build binary
 case $1 in
-  run)
-    cross build --target $TARGET;
+  deploy)
+    cross build --release --target $TARGET;
     scp ./target/$TARGET/debug/gpio2mqtt pi@$PI_IP:/home/pi;
+
+    # copy binary to /usr/local/bin
+    ssh pi@$PI_IP 'sudo install /home/pi/gpio2mqtt /usr/local/bin'
+
+    # copy config to /etc
+    scp ./gpio2mqtt.yaml pi@$PI_IP:/home/pi/gpio2mqtt.yaml
+    ssh pi@$PI_IP 'sudo install /home/pi/gpio2mqtt.yaml /etc'
+
+    # copy service to /etc/systemd/system
+    scp ./gpio2mqtt.service pi@$PI_IP:/home/pi/gpio2mqtt.service
+    ssh pi@$PI_IP 'sudo install /home/pi/gpio2mqtt.service /etc/systemd/system'
     ;;
   test)
     cross test --target=$TARGET --no-run --all
@@ -22,16 +33,5 @@ case $1 in
     ;;
 esac
 
-# copy binary to /usr/local/bin
-ssh pi@$PI_IP 'sudo install /home/pi/gpio2mqtt /usr/local/bin'
 
-# copy config to /etc
-scp ./gpio2mqtt.yaml pi@$PI_IP:/home/pi/gpio2mqtt.yaml
-ssh pi@$PI_IP 'sudo install /home/pi/gpio2mqtt.yaml /etc'
 
-# copy service to /etc/systemd/system
-scp ./gpio2mqtt.service pi@$PI_IP:/home/pi/gpio2mqtt.service
-ssh pi@$PI_IP 'sudo install /home/pi/gpio2mqtt.service /etc/systemd/system'
-
-# execute binary
-ssh pi@$PI_IP 'gpio2mqtt'
